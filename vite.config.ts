3c7b0778b81d302af32324d659ecf51a3c85969e
Vapoltavecs <vapoltavecs@gmail.com>
2023-06-24 20:31:02 +0400

    hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/CodeUnion-TestTask/",
+  base: "/",
   plugins: [
     react(),
     alias({
