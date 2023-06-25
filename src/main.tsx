a9309c6997805e8aafecc16b540f3024f5d82ec8
Vapoltavecs <vapoltavecs@gmail.com>
2023-06-25 20:56:41 +0400

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@app/App'
-import "@app/assets/index.sass"
+import "@app/assets/index.sass" 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
